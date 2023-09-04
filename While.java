7dda202f45133bd87dfed52957a01c5926a92b21
Rajashekar Bathine <rajashekarb0509@gmail.com>
2023-09-04 10:59:12 -0400

    for

--- While.java
+++ While.java
@@ -9,7 +9,7 @@ class While{
         i++;
         }
     
-        System.out.println("bye");
+       
         
     }
 }
---
+++ for.java
@@ -0,0 +1,11 @@
+public class For {
+
+    public static void main(String arg[]){
+
+
+        for(int i=2; i<=4; i++)
+
+        System.out.println(i);
+    }
+    
+}
