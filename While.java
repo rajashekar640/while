39f166b2395827416fc375d71f255aa49cacd1fd
Rajashekar Bathine <rajashekarb0509@gmail.com>
2023-09-04 10:55:14 -0400

    update

--- While.java
+++ While.java
@@ -1,12 +1,15 @@
 class While{
     public static void main(String arg[])
     {
-        int i = 1;
+        int i = 2;
 
         while(i<=8)
         {
-        System.out.println("hello" + i);
+        System.out.println(i);
         i++;
         }
+    
+        System.out.println("bye");
+        
     }
 }
---
+++ dowhile.java
@@ -0,0 +1,18 @@
+ class Dowhile {
+    public static void main (String arg[]){
+
+    
+
+    int i= 3;
+    do
+    {
+        System.out.println(i);
+        i++;
+
+    }
+    while(i<=4);
+
+    
+}
+ }
+
